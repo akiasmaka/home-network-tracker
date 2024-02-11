f6099ea53fa7745bc4929959adaeb626bfd77e38
akiasmaka <akiasmaka@gmail.com>
2024-02-11 22:28:21 +0000

    Grab ipv6 connections and add them to an ipv6 "stats" bpf map

--- probe/xdp.bpf.c
+++ probe/xdp.bpf.c
@@ -5,6 +5,7 @@
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
+#include <linux/ipv6.h>
 #include <linux/types.h>
 // clang-format on
 
@@ -16,13 +17,25 @@ typedef struct connection_stats {
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 10240);
-    __type(key, struct connection);
+    __type(key, struct ipv4_key);
     __type(value, connection_stats);
-} connection_tracker SEC(".maps");
+} ipv4_connection_tracker SEC(".maps");
 
-struct connection {
+struct ipv4_key {
     unsigned int saddr;
     unsigned int daddr;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, struct ipv6_key);
+    __type(value, connection_stats);
+} ipv6_connection_tracker SEC(".maps");
+
+struct ipv6_key {
+    struct in6_addr saddr;
+    struct in6_addr daddr;
 };
 
 SEC("xdp")
@@ -36,24 +49,26 @@ int xdp_count_type(struct xdp_md *ctx) {
         return XDP_DROP;
     }
 
-    //  Packet data from: https://stackoverflow.com/questions/58255831/xdp-program-ipheader-data-nh-off-confusion
-    //  | Ethernet     | IPv4               | IPv4 data (e.g. L4, data)       |
-    //  +--------------+--------------------+------ ... ----------------------+
-    //  ^              ^                    ^                                 ^
-    //  data           data + eth_offset    |                                 data_end
-    //                 iph                  |
-    //                 &iph[0]              &iph[1]
-    struct iphdr *iph = data + eth_offset;
-    if ((void *)&iph[1] > data_end) {
-        return XDP_ABORTED;
-    }
-
     uint16_t h_proto = eth->h_proto;
     if (h_proto == htons(ETH_P_IP)) {
-        struct connection new_connection = {iph->saddr, iph->daddr};
+        //  Packet data from: https://stackoverflow.com/questions/58255831/xdp-program-ipheader-data-nh-off-confusion
+        //  | Ethernet     | IPv4               | IPv4 data (e.g. L4, data)       |
+        //  +--------------+--------------------+------ ... ----------------------+
+        //  ^              ^                    ^                                 ^
+        //  data           data + eth_offset    |                                 data_end
+        //                 iph                  |
+        //                 &iph[0]              &iph[1]
+        struct iphdr *iph = data + eth_offset;
+        if ((void *)&iph[1] > data_end) {
+            return XDP_ABORTED;
+        }
+
+        // maybe don't count packets that have ttl < 1?
+
+        struct ipv4_key new_connection = {iph->saddr, iph->daddr};
         struct connection_stats *stats;
 
-        stats = bpf_map_lookup_elem(&connection_tracker, &new_connection);
+        stats = bpf_map_lookup_elem(&ipv4_connection_tracker, &new_connection);
         if (stats == NULL) {
             struct connection_stats new_stats = {0, 0};
 
@@ -61,18 +76,49 @@ int xdp_count_type(struct xdp_md *ctx) {
                        &iph->saddr,
                        &iph->daddr);
 
-            bpf_map_update_elem(&connection_tracker, &new_connection, &new_stats, BPF_NOEXIST);
+            bpf_map_update_elem(&ipv4_connection_tracker, &new_connection, &new_stats, BPF_NOEXIST);
         } else {
             (*stats).bytes += data_end - data;
             (*stats).packets++;
 
-            bpf_printk("Got packet to existing connection with source %pI4 and destination %pI4 this is packet number: %lu with a total %lu of bytes transferred",
-                       &iph->saddr,
-                       &iph->daddr,
+            // bpf_printk("Got packet to existing connection with source %pI4 and destination %pI4 this is packet number: %lu with a total %lu of bytes transferred",
+            //            &iph->saddr,
+            //            &iph->daddr,
+            //            (*stats).packets,
+            //            (*stats).bytes);
+
+            bpf_map_update_elem(&ipv4_connection_tracker, &new_connection, stats, BPF_EXIST);
+        }
+    } else if (h_proto == htons(ETH_P_IPV6)) {
+        struct ipv6hdr *ip6h = data + eth_offset;
+
+        if ((void *)&ip6h[1] > data_end) {
+            return XDP_ABORTED;
+        }
+
+        // maybe don't count packets that have hop_limit < 1?
+
+        struct ipv6_key new_connection = {ip6h->saddr, ip6h->daddr};
+        struct connection_stats *stats;
+        stats = bpf_map_lookup_elem(&ipv6_connection_tracker, &new_connection);
+        if (stats == NULL) {
+            struct connection_stats new_stats = {0, 0};
+
+            bpf_printk("Got packet to a new ipv6 connection with source %pI6 and destination %pI6",
+                       &ip6h->saddr,
+                       &ip6h->daddr);
+
+            bpf_map_update_elem(&ipv6_connection_tracker, &new_connection, &new_stats, BPF_NOEXIST);
+        } else {
+            (*stats).bytes += data_end - data;
+            (*stats).packets++;
+
+            bpf_printk("Got packet to existing ipv6 connection with source %pI6 and destination %pI6 this is packet number: %lu with a total %lu of bytes transferred",
+                       &ip6h->saddr,
+                       &ip6h->daddr,
                        (*stats).packets,
                        (*stats).bytes);
-
-            bpf_map_update_elem(&connection_tracker, &new_connection, stats, BPF_EXIST);
+            bpf_map_update_elem(&ipv6_connection_tracker, &new_connection, stats, BPF_EXIST);
         }
     }
 
